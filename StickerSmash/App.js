27ee8ba1a54814d635e6667990c8d6400d257954
Matt Gerig <mattgerig@gmail.com>
2024-01-27 09:37:09 -0600

    expo: Added image to the screen. Added new button components for different use cases.

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import ImageViewer from './components/imageViewer/image-viewer';
+import PrimaryButton from './components/primaryButton/primary-button';
+import Button from './components/button/button';
+
+import PlaceholderImage from "./assets/images/background-image.png"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <PrimaryButton label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
   },
 });
